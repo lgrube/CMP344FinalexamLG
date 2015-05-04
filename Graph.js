4b8993d46e6317af9610c2daf0817b1a285e02fe
lgrube <lgv9483@saintjoe.edu>
2015-05-04 12:51:13 -0500

    basic graph class

---
+++ Graph.js
@@ -0,0 +1,97 @@
+function Graph(v) {
+  this.vertices = v;
+  this.edges = 0;
+  this.adj = [];
+  for (var i = 0; i < this.vertices; ++i) {
+    this.adj[i] = [];
+    }
+  this.marked = [];
+  for (var i = 0; i < this.vertices; ++i) {
+    this.marked[i] = false;
+    }
+  this.addEdge = addEdge;
+  this.showGraph = showGraph;
+  this.dfs = dfs;
+  this.bfs = bfs;
+  this.edgeTo = [];
+  this.pathTo = pathTo;
+  this.hasPathTo = hasPathTo;
+  this.showPath = showPath;
+}
+
+function addEdge(v,w) {
+  this.adj[v].push(w);
+  this.adj[w].push(v);
+  this.edges++;
+}
+
+function showGraph() {
+  for (var i = 0; i < this.vertices; ++i) {
+    write(i + " -> ");
+    for (var j = 0; j < this.vertices; ++j) {
+      if (this.adj[i][j] != undefined)
+        write(this.adj[i][j] + ' ');
+    }
+    print();
+  }
+}
+
+function dfs(v) {
+  this.marked[v] = true;
+  if (this.adj[v] !== undefined) {
+    print("Visited vertex: " + v);
+    }
+  for (var i = 0; i < this.adj[v].length; i++) {
+    var w = this.adj[v][i];
+    if (!this.marked[w]) {
+      this.dfs(w);
+      }
+    }
+}
+
+function bfs(s) {
+	var queue = [];
+	this.marked[s] = true;
+	queue.push(s);
+	while(queue.length > 0) {
+		var v = queue.shift();
+		if(v !== undefinded) {
+			print("Visited vertex: " + v);
+		}
+		for (var i = 0; i < this.adj[v].length; i++) {
+			var w = this.adj[v][i];
+			if(!this.marked[w]) {
+				this.edgeTo[w] = v;
+				this.marked[w] = true;
+				queue.push(w);
+			}
+		}
+	}
+}
+
+function pathTo(source, v) {
+	if (!this.hasPathTo(v)) {
+		return undefined;
+	}
+	var path = [];
+	for (var i = v; i != source; i = this.edgeTo[i]) {
+		path.push(i);
+	}
+	path.push(source);
+	return path;
+}
+function hasPathTo(v) {
+	return this.marked[v];
+}
+
+function showPath(paths) {
+	while(paths.length > 0) {
+		if(paths.length > 1) {
+			print(paths.pop() + "-");
+		}
+		else {
+			print(paths.pop());
+		}
+	}
+}
+
